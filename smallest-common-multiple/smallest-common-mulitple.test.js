5c8e7aef01bd2f71e6db636c21cd0e533142aeb2
Alex Hall <alexpeterhall@gmail.com>
2021-04-07 12:55:00 -0400

    Comments the function

--- smallest-common-multiple/smallest-common-mulitple.test.js
+++ smallest-common-multiple/smallest-common-mulitple.test.js
@@ -1,20 +1,20 @@
 'use strict';
-const smallestCommonMultiple = require('./smallest-common-multiple');
+const smallest = require('./smallest-common-multiple');
 
 describe('Smallest Common Multiple Test Suite', () => {
   test('Should return 60', () => {
-    expect(smallestCommonMultiple.smallestCommon([1, 5])).toBe(60);
+    expect(smallest.smallestCommonMultiple([1, 5])).toBe(60);
   });
   test('Should return 60', () => {
-    expect(smallestCommonMultiple.smallestCommon([5, 1])).toBe(60);
+    expect(smallest.smallestCommonMultiple([5, 1])).toBe(60);
   });
   test('Should return 2520', () => {
-    expect(smallestCommonMultiple.smallestCommon([2, 10])).toBe(2520);
+    expect(smallest.smallestCommonMultiple([2, 10])).toBe(2520);
   });
   test('Should return 360360', () => {
-    expect(smallestCommonMultiple.smallestCommon([1, 13])).toBe(360360);
+    expect(smallest.smallestCommonMultiple([1, 13])).toBe(360360);
   });
   test('Should return 6056820', () => {
-    expect(smallestCommonMultiple.smallestCommon([23, 18])).toBe(6056820);
+    expect(smallest.smallestCommonMultiple([23, 18])).toBe(6056820);
   });
 });
--- smallest-common-multiple/smallest-common-multiple.js
+++ smallest-common-multiple/smallest-common-multiple.js
@@ -1,26 +1,41 @@
-function smallestCommon(arr) {
+'use strict';
+
+function smallestCommonMultiple(arr) {
+  // Uses destructuring to assign our two input elements from the argument array (sorted smallest to largest) to variables.
   const [smallMultiple, largeMultiple] = [...arr.sort((a, b) => a - b)];
+  // Flag to track if we've found our answer yet.
   let resultFound = false;
+  // Initializes result to start with our large multiple.
   let result = largeMultiple;
+  // Once we have a potential result that is evenly divisible by both the small and large multiples from our while loop below, we can start recursively calling isCommon() to check if it's also evenly divisible by all values between the small and large multiples.
   function isCommon(x) {
+    // If isCommon() is called with a value that has successfully decremented down to the smallMultiple, we've found our answer. This is our base case.
     if (x === smallMultiple) {
       resultFound = true;
       return;
-    } else if (result % x !== 0) {
+    }
+    // If our current potential result is not evenly divisible by one of the numbers between the small and large multiples, increment by largeMultiple, break the recursive call, and end up back in our while loop to keep searching.
+    else if (result % x !== 0) {
       result += largeMultiple;
       return;
-    } else {
+    }
+    // Value is a common multiple. Recursively call isCommon() decrementing by 1 to check remaining values.
+    else {
       return isCommon(x - 1);
     }
   }
+  // Keep running this until we've found our result.
   while (resultFound === false) {
+    // If our current potential "result" is also evenly divisible by the smallMultiple, we can start calling the isCommon() function to also check if it's evenly divisible by all values in between the small and large multiples.
     if (result % smallMultiple === 0) {
       isCommon(largeMultiple - 1);
-    } else {
+    }
+    // If our current potential "result" is not evenly divisible by the smallMultiple, increment by largeMultiple and try again.
+    else {
       result += largeMultiple;
     }
   }
   return result;
 }
 
-module.exports = { smallestCommon };
+module.exports = { smallestCommonMultiple };
