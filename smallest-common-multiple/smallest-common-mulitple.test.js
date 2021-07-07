938984b893bd373d75e27289627065bde8655d58
Alex Hall <alexpeterhall@gmail.com>
2021-07-07 11:15:26 -0400

    Cleaner variable names and input validation

--- smallest-common-multiple/smallest-common-mulitple.test.js
+++ smallest-common-multiple/smallest-common-mulitple.test.js
@@ -1,20 +1,46 @@
 'use strict';
-const smallest = require('./smallest-common-multiple');
+const validate = require('./smallest-common-multiple');
 
 describe('Smallest Common Multiple Test Suite', () => {
   test('Should return 60', () => {
-    expect(smallest.smallestCommonMultiple([1, 5])).toBe(60);
+    expect(validate.smallestCommonMultiple([1, 5])).toBe(60);
   });
   test('Should return 60', () => {
-    expect(smallest.smallestCommonMultiple([5, 1])).toBe(60);
+    expect(validate.smallestCommonMultiple([5, 1])).toBe(60);
   });
   test('Should return 2520', () => {
-    expect(smallest.smallestCommonMultiple([2, 10])).toBe(2520);
+    expect(validate.smallestCommonMultiple([2, 10])).toBe(2520);
   });
   test('Should return 360360', () => {
-    expect(smallest.smallestCommonMultiple([1, 13])).toBe(360360);
+    expect(validate.smallestCommonMultiple([1, 13])).toBe(360360);
   });
   test('Should return 6056820', () => {
-    expect(smallest.smallestCommonMultiple([23, 18])).toBe(6056820);
+    expect(validate.smallestCommonMultiple([23, 18])).toBe(6056820);
+  });
+  test('Should return Invalid Input warning for Number input', () => {
+    expect(validate.smallestCommonMultiple(123, 456)).toBe('Invalid Input. Expected an array with two number values.');
+  });
+  test('Should return Invalid Input warning for String input', () => {
+    expect(validate.smallestCommonMultiple('This is an invalid input')).toBe(
+      'Invalid Input. Expected an array with two number values.'
+    );
+  });
+  test('Should return Invalid Input warning for shorter than expected input array', () => {
+    expect(validate.smallestCommonMultiple([10])).toBe('Invalid Input. Expected an array with two number values.');
+  });
+  test('Should return Invalid Input warning for longer than expected input array', () => {
+    expect(validate.smallestCommonMultiple([10, 2, 20, 78])).toBe(
+      'Invalid Input. Expected an array with two number values.'
+    );
+  });
+  test('Should return Invalid Input warning for array with non-number values', () => {
+    expect(validate.smallestCommonMultiple(['Two', 'Ten'])).toBe(
+      'Invalid Input. Expected an array with two number values.'
+    );
+  });
+  test('Should return Invalid Input warning for array with non-number values', () => {
+    expect(validate.smallestCommonMultiple([['Two', 'Ten'], 10])).toBe(
+      'Invalid Input. Expected an array with two number values.'
+    );
   });
 });
--- smallest-common-multiple/smallest-common-multiple.js
+++ smallest-common-multiple/smallest-common-multiple.js
@@ -1,41 +1,40 @@
 'use strict';
 
-function smallestCommonMultiple(arr) {
-  // Uses destructuring to assign our two input elements from the argument array (sorted smallest to largest) to variables.
-  const [smallMultiple, largeMultiple] = [...arr.sort((a, b) => a - b)];
-  // Flag to track if we've found our answer yet.
+function smallestCommonMultiple(multiples) {
+  if (!Array.isArray(multiples) || multiples.length !== 2) {
+    return 'Invalid Input. Expected an array with two number values.';
+  }
+
+  const [smallMultiple, largeMultiple] = [...multiples.sort((a, b) => a - b)];
+
+  if (typeof smallMultiple !== 'number' || typeof largeMultiple !== 'number') {
+    return 'Invalid Input. Expected an array with two number values.';
+  }
+
   let resultFound = false;
-  // Initializes result to start with our large multiple.
-  let result = largeMultiple;
-  // Once we have a potential result that is evenly divisible by both the small and large multiples from our while loop below, we can start recursively calling isCommon() to check if it's also evenly divisible by all values between the small and large multiples.
-  function isCommon(x) {
-    // If isCommon() is called with a value that has successfully decremented down to the smallMultiple, we've found our answer. This is our base case.
-    if (x === smallMultiple) {
+  let currentValue = largeMultiple;
+
+  while (resultFound === false) {
+    if (currentValue % smallMultiple === 0) {
+      isCommon(largeMultiple - 1);
+    } else {
+      currentValue += largeMultiple;
+    }
+  }
+
+  function isCommon(num) {
+    if (num === smallMultiple) {
       resultFound = true;
       return;
-    }
-    // If our current potential result is not evenly divisible by one of the numbers between the small and large multiples, increment by largeMultiple, break the recursive call, and end up back in our while loop to keep searching.
-    else if (result % x !== 0) {
-      result += largeMultiple;
+    } else if (currentValue % num !== 0) {
+      currentValue += largeMultiple;
       return;
-    }
-    // Value is a common multiple. Recursively call isCommon() decrementing by 1 to check remaining values.
-    else {
-      return isCommon(x - 1);
+    } else {
+      return isCommon(num - 1);
     }
   }
-  // Keep running this until we've found our result.
-  while (resultFound === false) {
-    // If our current potential "result" is also evenly divisible by the smallMultiple, we can start calling the isCommon() function to also check if it's evenly divisible by all values in between the small and large multiples.
-    if (result % smallMultiple === 0) {
-      isCommon(largeMultiple - 1);
-    }
-    // If our current potential "result" is not evenly divisible by the smallMultiple, increment by largeMultiple and try again.
-    else {
-      result += largeMultiple;
-    }
-  }
-  return result;
+
+  return currentValue;
 }
 
 module.exports = { smallestCommonMultiple };
