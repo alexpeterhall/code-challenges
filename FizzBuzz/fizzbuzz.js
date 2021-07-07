2becc560456be29c049ea877af95c9fafb5d7a2e
Alex Hall <alexpeterhall@gmail.com>
2021-07-07 11:11:48 -0400

    Removes old console.logs

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -6,22 +6,16 @@ function fizzbuzz() {
     if (i % 3 === 0) {
       if (i % 5 === 0) {
         result.push('FizzBuzz');
-        //console.log('FizzBuzz');
       } else {
         result.push('Fizz');
-        //console.log('Fizz');
       }
     } else if (i % 5 === 0) {
       result.push('Buzz');
-      //console.log('Buzz');
     } else {
       result.push(i);
-      //console.log(i);
     }
   }
   return result;
 }
-
-//fizzbuzz();
 
 module.exports = { fizzbuzz };
