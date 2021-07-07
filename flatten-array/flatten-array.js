1eafa7f01fe4b81ba349a5a13c8010c9406dd915
Alex Hall <alexpeterhall@gmail.com>
2021-07-07 11:12:36 -0400

    Removes unnecessary comment

--- flatten-array/flatten-array.js
+++ flatten-array/flatten-array.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function flattenArray(inputArray) {
-  // Uses the reduce() method to "map" through all elements and concatenate result to the accumulator
   return inputArray.reduce((accumulator, element) => {
-    /* Ternary statement checks if the current element is itself an array. If it is, it recursively calls the flattenArray function to flatten it. If the element value is not an array it concatenates the value to our accumulator array. */
+    /* Ternary statement checks if the current element is itself an array. If it is, it recursively calls the flattenArray function to flatten it. If the element value is not an array it concatenates the value to our accumulator array which is ultimately returned as the result. */
     return accumulator.concat(Array.isArray(element) ? flattenArray(element) : element);
   }, []);
 }
