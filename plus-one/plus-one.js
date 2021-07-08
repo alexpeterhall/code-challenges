8f6bdf0f5e329348794d23398bf2f9099763e725
Alex Hall <alexpeterhall@gmail.com>
2021-07-07 21:26:37 -0400

    Cleans names and removes comments

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -1,44 +1,38 @@
+'use strict';
+
 /**
- * @param {number[]} digits
+ * @param {number[]} numbers
  * @return {number[]}
  */
-const plusOne = function (digits) {
-  let count = 0;
+
+const plusOne = function (numbers) {
+  let numberOfTrailingNines = 0;
   const result = [];
-  // Start at the end of the array and find out how many trailing digits are 9s
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] === 9) {
-      count++;
+
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    if (numbers[i] === 9) {
+      numberOfTrailingNines++;
     } else {
       break;
     }
   }
-  // If there is only 1 digit in the array and it's not a 9 just increment it and push to the result array.
-  if (digits.length === 1 && digits[0] !== 9) {
-    result.push(digits[0] + 1);
-  }
-  // If every digit in the array is a 9 they all need to roll over to 0's and add a leading 1.
-  else if (count === digits.length) {
+
+  if (numbers.length === 1 && numbers[0] !== 9) {
+    result.push(numbers[0] + 1);
+  } else if (numberOfTrailingNines === numbers.length) {
     result.push(1);
-    for (let j = 0; j < count; j++) {
+    for (let i = 0; i < numberOfTrailingNines; i++) {
       result.push(0);
     }
-  }
-  // If we have some trailing 9s
-  else if (count > 0 && count < digits.length) {
-    // Push the first section of the array up to but not including our trailing 9s
-    result.push(...digits.slice(0, digits.length - count));
-    // Increment the last digit on the first subsection
-    result[result.length - 1] = result[result.length - 1] + 1;
-    // Replace the trailing 9s with 0s
-    for (let j = 0; j < count; j++) {
+  } else if (numberOfTrailingNines > 0 && numberOfTrailingNines < numbers.length) {
+    result.push(...numbers.slice(0, numbers.length - numberOfTrailingNines));
+    result[result.length - 1] += 1;
+    for (let i = 0; i < numberOfTrailingNines; i++) {
       result.push(0);
     }
-  }
-  // Push a copy of the whole input array onto the result array and increment the last digit
-  else {
-    result.push(...digits);
-    result[result.length - 1] = result[result.length - 1] + 1;
+  } else {
+    result.push(...numbers);
+    result[result.length - 1] += 1;
   }
   return result;
 };
--- plus-one/plus-one.test.js
+++ plus-one/plus-one.test.js
@@ -1,35 +1,35 @@
 'use strict';
-const plusOne = require('./plus-one');
+const validate = require('./plus-one');
 
 describe('Plus One Test Suite', () => {
   test('[0] should return [1]', () => {
-    expect(plusOne.plusOne([0])).toEqual([1]);
+    expect(validate.plusOne([0])).toEqual([1]);
   });
   test('[1] should return [2]', () => {
-    expect(plusOne.plusOne([1])).toEqual([2]);
+    expect(validate.plusOne([1])).toEqual([2]);
   });
   test('[9] should return [1,0]', () => {
-    expect(plusOne.plusOne([9])).toEqual([1, 0]);
+    expect(validate.plusOne([9])).toEqual([1, 0]);
   });
   test('[9,9] should return [1,0,0]', () => {
-    expect(plusOne.plusOne([9, 9])).toEqual([1, 0, 0]);
+    expect(validate.plusOne([9, 9])).toEqual([1, 0, 0]);
   });
   test('[9,9,9,9,9] should return [1,0,0,0,0,0]', () => {
-    expect(plusOne.plusOne([9, 9, 9, 9, 9])).toEqual([1, 0, 0, 0, 0, 0]);
+    expect(validate.plusOne([9, 9, 9, 9, 9])).toEqual([1, 0, 0, 0, 0, 0]);
   });
   test('[1,2,3] should return [1,2,4]', () => {
-    expect(plusOne.plusOne([1, 2, 3])).toEqual([1, 2, 4]);
+    expect(validate.plusOne([1, 2, 3])).toEqual([1, 2, 4]);
   });
   test('[9,2,3,4,5] should return [9,2,3,4,6]', () => {
-    expect(plusOne.plusOne([9, 2, 3, 4, 5])).toEqual([9, 2, 3, 4, 6]);
+    expect(validate.plusOne([9, 2, 3, 4, 5])).toEqual([9, 2, 3, 4, 6]);
   });
   test('[5,3,9,6,3,9,2] should return [5,3,9,6,3,9,3]', () => {
-    expect(plusOne.plusOne([5, 3, 9, 6, 3, 9, 2])).toEqual([5, 3, 9, 6, 3, 9, 3]);
+    expect(validate.plusOne([5, 3, 9, 6, 3, 9, 2])).toEqual([5, 3, 9, 6, 3, 9, 3]);
   });
   test('[1, 2, 3, 4, 5, 9, 9] should return [1, 2, 3, 4, 6, 0, 0]', () => {
-    expect(plusOne.plusOne([1, 2, 3, 4, 5, 9, 9])).toEqual([1, 2, 3, 4, 6, 0, 0]);
+    expect(validate.plusOne([1, 2, 3, 4, 5, 9, 9])).toEqual([1, 2, 3, 4, 6, 0, 0]);
   });
   test('[1, 2, 3, 4, 5, 9, 9, 9, 9, 9] should return [1, 2, 3, 4, 6, 0, 0, 0, 0, 0]', () => {
-    expect(plusOne.plusOne([1, 2, 3, 4, 5, 9, 9, 9, 9, 9])).toEqual([1, 2, 3, 4, 6, 0, 0, 0, 0, 0]);
+    expect(validate.plusOne([1, 2, 3, 4, 5, 9, 9, 9, 9, 9])).toEqual([1, 2, 3, 4, 6, 0, 0, 0, 0, 0]);
   });
 });
