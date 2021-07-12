de8c8b6e5d7c41dd528c2ca79de16e7cea2003da
Alex Hall <alexpeterhall@gmail.com>
2021-07-12 19:54:49 -0400

    Cleans up code

--- rot13-cipher/rot13-cipher.js
+++ rot13-cipher/rot13-cipher.js
@@ -1,29 +1,27 @@
 'use strict';
 
 function decodeROT13Cipher(encodedString) {
-  const encodedArray = [...encodedString];
-
-  function decodeCharacter(char) {
-    const charCode = char.charCodeAt(0);
-    // Unicode decimal representing first 13 characters of the alphabet in ALL CAPS
-    if (charCode >= 65 && charCode <= 77) {
-      return String.fromCharCode(charCode + 13);
-    }
-    // Unicode decimal representing last 13 characters of the alphabet in ALL CAPS
-    else if (charCode >= 78 && charCode <= 90) {
-      return String.fromCharCode(charCode - 13);
-    }
-    // Maintain other characters as is (spaces, punctuation, etc.)
-    else {
-      return String.fromCharCode(charCode);
-    }
+  let result = '';
+  for (const character of encodedString) {
+    result += decodeCharacter(character);
   }
+  return result;
+}
 
-  const result = encodedArray.map((element) => {
-    return decodeCharacter(element);
-  });
-
-  return result.join('');
+function decodeCharacter(character) {
+  const charCode = character.charCodeAt(0);
+  // Unicode decimal representing first 13 characters of the alphabet in ALL CAPS
+  if (charCode >= 65 && charCode <= 77) {
+    return String.fromCharCode(charCode + 13);
+  }
+  // Unicode decimal representing last 13 characters of the alphabet in ALL CAPS
+  else if (charCode >= 78 && charCode <= 90) {
+    return String.fromCharCode(charCode - 13);
+  }
+  // Maintain other characters as is (spaces, punctuation, etc.)
+  else {
+    return String.fromCharCode(charCode);
+  }
 }
 
 module.exports = { decodeROT13Cipher };
